6bcb875a663bda629af8c0b8fd110aef06b6dc33
a-newcoder <email@example.com>
2023-06-22 08:27:50 +0800

    web

--- web/服务器/try.py
+++ web/服务器/try.py
@@ -1,5 +1,10 @@
-
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "hallo,world"
 
 
 if __name__ == '__main__':
-    pass
+
+    app.run(host='0.0.0.0', port=80)
